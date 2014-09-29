e9ebaff28b323790ce173df9ea1787ce38cebe8e
Viktor <victor.pishuk@gmail.com>
2014-09-29 12:26:53 +0300

    prepare initial html and css for movements

--- app/app.js
+++ app/app.js
@@ -1,13 +1,4 @@
 (function () {
     "use strict";
-    // Each component will define it's own translations as a property of the
-    // `i18nTranslations` global object. This needs to be defined here before any
-    // module.
-    angular.module("app", ["ngRoute", "ui.bootstrap"]);
-
-    angular.module("app").run(function () {
-    });
-
-    angular.module("app").config(function () {
-    });
+    angular.module("myApp", []);
 })();
