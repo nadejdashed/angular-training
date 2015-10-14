ababbea2389014ed607ae9a2686ef632ec7a5f75
Nadzeya.Shedava <Nadzeya.Shedava@aerlingus.com>
2015-10-15 01:17:29 +0300

    Add rest server

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 var expressIO = require('express.io'),
-    serveStatic = require('serve-static');
+    serveStatic = require('serve-static'),
+    fs = require('fs'),
+    extend = require('util')._extend;
 
 var app = expressIO(),
-  folder = process.argv[2] !== 'debug' ? 'build' : 'app';
+  folder = process.argv[2] !== 'debug' ? 'build' : 'app',
+  eventFile = './json/events.json';
 
 app.use(expressIO.cookieParser());
 app.use(expressIO.session({secret: 'monkey'}));
+app.use(expressIO.bodyParser());
 
 app.http().io();
 app.listen(8000);
@@ -21,9 +25,69 @@ app.get('/templates/{name}', function(req, res) {
 app.use(expressIO.static(__dirname + '/'));
 app.use(expressIO.static(__dirname + '/' + folder));
 
-app.get('/cats', function(req, res) {
-    var result = require('./json/cats.json');
+app.get('/events', function(req, res) {
+    var result = require(eventFile);
     res.json(result);
+});
+app.get('/events/:id', function(req, res) {
+    var result = require(eventFile),
+      id = req.params.id,
+      event = result.responses.filter(function(el){return el.id == id})[0];
+    res.json(event);
+});
+app.post('/events', function(req, res){
+    var result = require(eventFile),
+      events = result.responses,
+      lastId = events[events.length - 1].id,
+      data = req.body;
+
+    data.id = lastId + 1;
+    data.vote = 0;
+    data.src = data.src || "";
+    data.date = new Date();
+
+    result.responses.push(data);
+    fs.writeFile(eventFile, JSON.stringify(result), function(err) {
+        console.log(err ? err : "JSON saved to " + eventFile);
+        if (err){
+            res.error(err);
+        } else {
+            res.send(result);
+        }
+    });
+});
+app.put('/events/:id', function(req, res){
+    var id = req.params.id,
+      result = require(eventFile),
+      event = result.responses.filter(function(el){return el.id == id})[0],
+      data = req.body;
+
+    extend(event, data);
+    fs.writeFile(eventFile, JSON.stringify(result), function(err) {
+        console.log(err ? err : "JSON saved to " + eventFile);
+        if (err){
+            res.error(err);
+        } else {
+            res.send(result);
+        }
+    });
+});
+app.delete('/events/:id', function(req, res){
+    var id = req.params.id,
+      result = require(eventFile),
+      event = result.responses.filter(function(el){return el.id == id})[0],
+      events = result.responses,
+      ind = events.indexOf(event);
+
+    if (ind >= 0) {events.splice(ind, 1);}
+    fs.writeFile(eventFile, JSON.stringify(result), function(err) {
+        console.log(err ? err : "JSON saved to " + eventFile);
+        if (err){
+            res.error(err);
+        } else {
+            res.send(result);
+        }
+    });
 });
 
 exports = module.exports = app;
