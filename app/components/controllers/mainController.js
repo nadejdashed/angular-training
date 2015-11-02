f985df3579e6af352fec1c68efd0d92a984e9b71
Oleksandr Kiporenko <oleksandr_kiporenko@epam.com>
2015-11-02 18:33:25 +0200

    Day1: Cat Clicker Premium

--- app/components/controllers/mainController.js
+++ app/components/controllers/mainController.js
@@ -1,7 +1,18 @@
 ﻿(function(module) {
 
     var mainController = function ($scope) {
-        $scope.text = "Hello World!";
+        $scope.cats = [{name:'John', image:'Artefacts/cat1.jpeg', clickedCount:0},
+            {name:'Mary', image:'Artefacts/cat2.jpeg', clickedCount:0},
+            {name:'Mike', image:'Artefacts/cat3.jpeg', clickedCount:0},
+            {name:'Adam', image:'Artefacts/cat4.jpeg', clickedCount:0},
+            {name:'Julie', image:'Artefacts/cat5.jpeg', clickedCount:0}];
+        $scope.selectedCat = $scope.cats[0];
+        $scope.catWasClicked = function (cat) {
+            cat.clickedCount++;
+        }
+        $scope.catWasSelected = function (cat) {
+            $scope.selectedCat = cat;
+        }
     };
 
     module.controller("mainController", mainController);
