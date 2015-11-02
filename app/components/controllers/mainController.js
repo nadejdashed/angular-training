93dd0920bc79c49a725470e0cc354c1b7665eb20
pashaOliinyk <pashades@gmail.com>
2015-11-02 17:08:20 +0200

    stage3 done

--- app/components/controllers/mainController.js
+++ app/components/controllers/mainController.js
@@ -9,15 +9,38 @@
 
             { name: 'dru',
               img:'http://www.factslides.com/imgs/black-cat.jpg',
+              clicks: 0},
+
+            {name: 'bru',
+            img:'https://statswithcats.files.wordpress.com/2012/07/claws-cool-cat-picture-21-b.jpg',
+            clicks: 0},
+
+            { name: 'bro',
+              img:'http://media4.popsugar-assets.com/files/2014/09/19/978/n/1922507/4bc5042ee37fa1f9_thumb_temp_cover_file13465311411161397.xxxlarge/i/Funny-Cat-Costumes.jpg',
+              clicks: 0},
+
+            { name: 'hru',
+              img:'http://www.andrew.cmu.edu/user/cfperron/cats/images/cat8.jpg',
               clicks: 0}];
 
+        $scope.currentCat ={
+            name: '',
+            img:'',
+            clicks: 0};
+
         $scope.counterTotal = 0;
+
         $scope.incCounterPicture = function(_index){
             $scope.images[_index].clicks++;
             console.log('hit btn['+_index+']', $scope.counter);
             //
             $scope.counterTotal++;
             //
+            $scope.currentCat = {
+                name: $scope.images[_index].name,
+                img: $scope.images[_index].img,
+                clicks: $scope.images[_index].clicks};
+
             return $scope.images[_index].clicks;
         };
     };
