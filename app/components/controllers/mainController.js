0b77e8b871ee18d4c00e91ac951a11ed0a010162
pashaOliinyk <pashades@gmail.com>
2015-11-02 16:06:34 +0200

    step2-done

--- app/components/controllers/mainController.js
+++ app/components/controllers/mainController.js
@@ -2,6 +2,15 @@
 
     var mainController = function ($scope) {
         $scope.text = "Hello World!";
+        $scope.images = ['http://25.media.tumblr.com/tumblr_lncvc9SMfW1qbe5pxo1_500.jpg',
+                         'http://www.factslides.com/imgs/black-cat.jpg'];
+
+        $scope.counter = 0;
+        $scope.incCounterPicture = function(){
+            $scope.counter++;
+            console.log('hit btn',$scope.counter);
+            return $scope.counter;
+        };
     };
 
     module.controller("mainController", mainController);
