ec9e1a1e5b7a1268ad69d3649d84b8b9c1aa3e59
Artem_Lazurenko <Artem_Lazurenko@epam.com>
2015-11-02 17:18:04 +0200

    Implemented premium cat clicker.

--- app/components/controllers/mainController.js
+++ app/components/controllers/mainController.js
@@ -1,8 +1,24 @@
 ﻿(function(module) {
 
     var mainController = function ($scope) {
-        $scope.text = "Hello World!";
-    };
+        $scope.cats = [
+            {name: "Kitty", url: "http://i.bigmir.net/img/dnevnik/uploads/cmu_1153/29306/1.jpg", counter: 0},
+            {name: "Pussy", url: "https://upload.wikimedia.org/wikipedia/uk/c/cf/Black-cat.jpg", counter: 0},
+            {name: "Bella", url: "http://cuteness-test.s3.amazonaws.com/styles/profile_crop/s3/pet-photos/9942_4c1047ed74f3c_lg.jpg?itok=8VLsgv-X", counter: 0},
+            {name: "Chloe", url: "http://cuteness-test.s3.amazonaws.com/styles/profile_crop/s3/pet-photos/18770_5096adaa3aca3_lg.jpg?itok=i9r4AAmg", counter: 0},
+            {name: "Jaspr", url: "http://cuteness-test.s3.amazonaws.com/styles/profile_crop/s3/pet-photos/1044_49f4ceb454caa_lg.jpg?itok=B8iibRqw", counter: 0}
+        ]
+
+        $scope.incrementCounter = function(cat) {
+            cat.counter++;
+        }
+
+        $scope.setCurrCat = function (cat) {
+            $scope.currCat = cat;
+        }
+
+        $scope.currCat = $scope.cats[0];
+    }
 
     module.controller("mainController", mainController);
 
