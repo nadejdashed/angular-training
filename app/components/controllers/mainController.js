8614eb92899f59341e12b7962bb67a0c2ba90425
Maksym Lisevych <maxim.lisevich@gmail.com>
2015-11-02 18:28:35 +0200

    First home task

--- app/components/controllers/mainController.js
+++ app/components/controllers/mainController.js
@@ -1,9 +1,21 @@
 ﻿(function(module) {
 
     var mainController = function ($scope) {
-        $scope.text = "Hello World!";
+    	$scope.cats = [{image:"cat.png", name:"cat", clickNumber:0}, {image:"cat2.png", name:"cat2", clickNumber:0}, {image:"cat3.png", name:"cat3", clickNumber:0}];
+    	$scope.cat = {};
+
+    	$scope.selectCat = function selectCat(cat)
+    	{
+    		$scope.cat = cat;
+    	}
+
+    	$scope.imgClick = function imgClick(cat)
+    	{
+    		$scope.cat.clickNumber ++;
+    	}
     };
 
     module.controller("mainController", mainController);
 
 }(angular.module("app")));
+
