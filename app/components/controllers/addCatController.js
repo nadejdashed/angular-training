cc21b0f1eec75c3664ad1005194573503c793d94
Oleksandr Kiporenko <oleksandr_kiporenko@epam.com>
2015-11-03 19:27:50 +0200

    Day2: Step 2. Add a search box and orderBy fields

---
+++ app/components/controllers/addCatController.js
@@ -0,0 +1,14 @@
+﻿(function(module) {
+
+    var addCatController = function ($scope) {
+        $scope.submit = function() {
+            if ($scope.text) {
+                $scope.list.push(this.text);
+                $scope.text = '';
+            }
+        };
+    };
+
+    module.controller("addCatController", addCatController);
+
+}(angular.module("app")));
---
+++ app/components/controllers/voteController.js
@@ -0,0 +1,61 @@
+﻿(function(module) {
+
+    var voteController = function ($scope) {
+        var didSelectcatFunction = function (cat) {
+            $scope.selectedCat = cat;
+            cat.wasAlreadyViewed = true;
+        };
+
+        $scope.cats = [
+            {
+                name:'John',
+                image:'Artefacts/cat1.jpeg',
+                vote:0,
+                wasAlreadyViewed:false
+            },
+            {
+                name:'Mary',
+                image:'Artefacts/cat2.jpeg',
+                vote:0,
+                wasAlreadyViewed:false
+            },
+            {
+                name:'Mike',
+                image:'Artefacts/cat3.jpeg',
+                vote:0,
+                wasAlreadyViewed:false
+            },
+            {
+                name:'Adam',
+                image:'Artefacts/cat4.jpeg',
+                vote:0,
+                wasAlreadyViewed:false
+            },
+            {
+                name:'Julie',
+                image:'Artefacts/cat5.jpeg',
+                vote:0,
+                wasAlreadyViewed:false
+            }
+        ];
+
+        $scope.voteUpForCat = function (cat) {
+            cat.vote++;
+        };
+
+        $scope.voteDownForCat = function (cat) {
+            cat.vote--;
+        };
+
+        $scope.applyFilter = function (newFilterText) {
+            $scope.filterText = newFilterText;
+        };
+
+        $scope.didSelectCat = didSelectcatFunction;
+
+        didSelectcatFunction($scope.cats[0]);
+    };
+
+    module.controller("voteController", voteController);
+
+}(angular.module("app")));
