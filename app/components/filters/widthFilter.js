91720374ec770f7bedfdf11541006c67bd99bcde
pashaOliinyk <pashades@gmail.com>
2015-11-03 18:22:47 +0200

    add checker-icon for buttons

--- app/components/filters/widthFilter.js
+++ app/components/filters/widthFilter.js
@@ -1,11 +1,16 @@
 ﻿(function(module) {
 
-    
+    var checker = function() {
+        return function(input) {
+            return input ? '\u2713' : '\u2718';
+        };
+    };
+
     var widthFilter = function ($filter, $sce) {
         return function (input, options) {
             var result = $filter('emoticons')(input, options);
             var htmlFragmentText = $sce.getTrustedHtml(result);
-            var el = document.createElement("div");
+              var el = document.createElement("div");
             el.innerHTML = htmlFragmentText;
 
             var matches = el.querySelectorAll("i.emoticon, i.icon-emoticon");
@@ -18,6 +23,7 @@
         }
     }
 
+    module.filter('checker', checker);
     module.filter("widthFilter", widthFilter);
 
 }(angular.module("app")));
