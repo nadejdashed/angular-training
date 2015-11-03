4d5c20a670ef98eca9e9119fae90dba098157088
Maksym Lisevych <maxim.lisevich@gmail.com>
2015-11-03 18:53:26 +0200

    2 lesson

--- app/components/controllers/mainController.js
+++ app/components/controllers/mainController.js
@@ -1,17 +1,26 @@
 ﻿(function(module) {
 
     var mainController = function ($scope) {
-    	$scope.cats = [{image:"cat.png", name:"cat", clickNumber:0}, {image:"cat2.png", name:"cat2", clickNumber:0}, {image:"cat3.png", name:"cat3", clickNumber:0}];
+    	$scope.cats = [{image:"cat1.png", name:"cat1", visited: "false"}, {image:"cat2.png", name:"cat2", visited: "false"}, {image:"cat3.png", name:"cat3", visited: "false"}, {image:"cat4.png", name:"cat4", visited: "false"}, {image:"cat5.png", name:"cat5", visited: "false"}];
     	$scope.cat = {};
+    	$scope.showSpinner = false;
+    	$scope.sortWay = false;
+
+    	$scope.options = [{ name: "Straight", id: 0 }, { name: "Reverse", id: 1 }];
+		$scope.option = {};
+		$scope.selectedOption = $scope.options[0];
 
     	$scope.selectCat = function selectCat(cat)
     	{
     		$scope.cat = cat;
+    		$scope.cat.viewed = true;
+    		$scope.cat.checkmark = "\u2713";
     	}
 
-    	$scope.imgClick = function imgClick(cat)
+    	$scope.doSearch = function doSearch()
     	{
-    		$scope.cat.clickNumber ++;
+    		$scope.tmpSearchVariable = $scope.inputSearch;
+    		$scope.showSpinner = true;
     	}
     };
 
