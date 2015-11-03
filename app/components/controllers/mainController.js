7d6ae6f665bb7d6b5b0322d21076cc7023643102
Kateryna Obertynska <kateryna.obertynska@icloud.com>
2015-11-03 15:41:25 +0200

    First part is added

--- app/components/controllers/mainController.js
+++ app/components/controllers/mainController.js
@@ -1,7 +1,21 @@
 ﻿(function(module) {
 
     var mainController = function ($scope) {
-        $scope.text = "Hello World!";
+
+        var cats = [{name: "Tom", imageURL: "http://stylonica.com/wp-content/uploads/2014/04/Cat-Wallpaper.jpg", clicksCount: 0},
+            {name: "Jeck", imageURL: "http://cdn.desktopwallpapers4.me/media/thumbs_400x250/3/27967.jpg", clicksCount: 0}];
+
+        $scope.cats = cats;
+        $scope.currentCat = cats[0];
+
+       $scope.selectCat = function (cat) {
+
+           $scope.currentCat = cat;
+       }
+
+        $scope.didClickedCat = function (cat) {
+            cat.clicksCount ++;
+        }
     };
 
     module.controller("mainController", mainController);
