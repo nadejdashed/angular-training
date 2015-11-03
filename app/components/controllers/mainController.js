7373e8fc965f303ce1dc3a1f386ca76454768b55
mtauzhnianskyi <Mykhaylo_Tauzhnianskyi@epam.com>
2015-11-03 19:11:28 +0200

    additional steps

--- app/components/controllers/mainController.js
+++ app/components/controllers/mainController.js
@@ -1,24 +1,46 @@
 ﻿(function(module) {
 
     var mainController = function ($scope) {
-        $scope.text = "Hello World!!!";
+        $scope.orderBySelected= null;
+        $scope.cat = null;
+        $scope.cats = [
+            {cat_name:"Tom",
+                link: "../im/1_cat.png",
+                click_num:0},
+            {cat_name:"Jerry cat",
+                link: "../im/2_cat.png",
+                click_num:0},
+            {cat_name:"Jerry cat 1",
+                link: "../im/3_cat.png",
+                click_num:0},
+            {cat_name:"Jerry cat 2",
+                link: "http://gazettereview.com/wp-content/uploads/2015/05/cat.jpg",
+                click_num:0},
+            {cat_name:"Jerry cat 3",
+                link: "https://www.petfinder.com/wp-content/uploads/2012/11/140272627-grooming-needs-senior-cat-632x475.jpg",
+                click_num:0}
+        ];
 
-        $scope.image_path_1 = "../im/1_cat.png";
-
-        $scope.image_path_2 = "../im/2_cat.png";
-
-        $scope.first_cat_name = "Tom";
-        $scope.second_cat_name = "Jerry cat";
-
-        $scope.first_click_counter =0;
-        $scope.second_click_counter =0;
-
-        $scope.firstCountClick = function(){
-            $scope.first_click_counter++;
+        $scope.countClick = function(cat){
+            cat.click_num++;
         }
-        $scope.secondCountClick = function(){
-            $scope.second_click_counter++;
+        $scope.selectCat = function(cat){
+            $scope.cat = cat;
+            $scope.cat.is_viewed = true;
         }
+
+        $scope.plusClick = function(cat){
+            $scope.countClick(cat);
+        }
+        $scope.minusClick = function(cat){
+            cat.click_num--;
+        }
+
+        $scope.searchClick = function(search){
+            $scope.searchCat = {cat_name:search};
+
+        }
+
     };
 
     module.controller("mainController", mainController);
