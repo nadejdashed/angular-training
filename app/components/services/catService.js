24bf1619ba1d00790703a09902d9ea87ddc71596
Artem_Lazurenko <Artem_Lazurenko@epam.com>
2015-11-04 15:30:36 +0200

    Added catService.js

---
+++ app/components/services/catService.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Artem_Lazurenko on 03.11.2015.
+ */
+(function(module) {
+
+    var catService = function ($scope, $q) {
+    };
+
+    module.factory("catService", catService);
+
+}(angular.module("app")));
