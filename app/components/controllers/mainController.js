b49fb63cc164dcc0d741d5f4bde9676d1ca547c7
Pavlo Komenda <Pavlo_Komenda@epam.com>
2015-11-06 17:08:49 +0200

    [STEP 3][HTML/JS][CTRL] Cat Clicker Premium

--- app/components/controllers/mainController.js
+++ app/components/controllers/mainController.js
@@ -12,10 +12,30 @@
                 name: 'Dymok',
                 src: picPath + '2.jpg',
                 clickQuantity: 0
+            },
+            {
+                name: 'Tom',
+                src:  picPath + '3.jpg',
+                clickQuantity: 0
+            },
+            {
+                name: 'Snowball',
+                src:  picPath + '4.jpg',
+                clickQuantity: 0
+            },
+            {
+                name: 'Gremm',
+                src: picPath + '5.jpg',
+                clickQuantity: 0
             }
         ];
+        $scope.selectedCat = $scope.cats[1];
+
         $scope.getIncrement = function (obj) {
             return obj.clickQuantity += 1;
+        };
+        $scope.selectThis = function (obj) {
+            $scope.selectedCat = obj;
         };
     };
 
