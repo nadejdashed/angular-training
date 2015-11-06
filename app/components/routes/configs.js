46b45399f60ea1f9253536ed2e626bbe3b4f6dd0
mtauzhnianskyi <Mykhaylo_Tauzhnianskyi@epam.com>
2015-11-06 16:44:07 +0200

    first config

--- app/components/routes/configs.js
+++ app/components/routes/configs.js
@@ -1,4 +1,42 @@
 /**
  * Created by Mykhaylo_Tauzhniansk on 11/6/2015.
  */
-module.config()
+angular.module("app").config(function($stateProvider, $urlRouterProvider, $locationProvider){
+    $locationProvider.html5Mode(true);
+
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider.state('catslist', {
+        url: '/',
+        views: {
+            about: {
+                template: 'About me page'
+            },
+            "": {
+                templateUrl: '/templates/main_cats_page.html',
+                controller: 'mainController',
+                resolve: {
+                    events: function(catsService){
+                        return catsService.getData();
+                    }
+                }
+            }
+        }
+    }).state('events.edit', {
+        url: 'edit/:id',
+        templateUrl: '/templates/edit.html',
+        controller: 'editController',
+        resolve: {
+            cat: function($state, $stateParams, eventsService){
+                return catsService.getEventById($stateParams.id);
+            }
+        }
+    }).state('cats', {
+        url: '/',
+        templateUrl: '/templates/main_cats_page.html',
+        controller: 'mainController'
+    }).state('about', {
+        url: '/aboutme',
+        template: 'About me page'
+    });
+});
