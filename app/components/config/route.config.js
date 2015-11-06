0bf415d044fa6105bc1e937a0555a2b0fcb17689
Artem_Lazurenko <Artem_Lazurenko@epam.com>
2015-11-06 17:30:14 +0200

    Routing broken.

---
+++ app/components/config/route.config.js
@@ -0,0 +1,28 @@
+angular.module("app").config(function($routeProvider, $locationProvider){
+
+    $locationProvider.html5Mode(true);
+
+    $routeProvider.when('/', {
+        controller: 'mainController',
+        templateUrl: '/templates/main.html',
+        resolve: {
+            events: function(catService, $q, $timeout){
+                var defer = $q.defer();
+                $timeout(function(){
+                    catService.getData().then(function(data){
+                        defer.resolve(data);
+                    });
+                }, 2000);
+                return defer.promise;
+            }
+        }
+    }).when('/add-cat', {
+        controller: 'addCatController',
+        templateUrl: '/templates/add-cat.html'
+    }).when('/about', {
+        template: 'My about'
+    }).otherwise({
+        redirectTo: '/'
+    });
+
+});
