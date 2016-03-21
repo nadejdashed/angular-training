3b4ad775440b84f76d61131eb1a26812c64043d3
Nadzeya.Shedava <nadzeya_shedava@epam.com>
2016-03-21 22:15:57 +0300

    Simplify grunt

--- app/components/controllers/mainController.js
+++ app/components/controllers/mainController.js
@@ -1,7 +1,7 @@
 ﻿(function(module) {
 
     var mainController = function ($scope) {
-        $scope.text = "Hello World!";
+        $scope.text = "Hello World?";
     };
 
     module.controller("mainController", mainController);
