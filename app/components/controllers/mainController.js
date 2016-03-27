7cf18b7f0f28c3725328a2f8e7839de334402686
Alex.Semenk <Alex.Semenk@gmail.com>
2016-03-27 22:45:46 +0300

    First and half of the second task is completed

--- app/components/controllers/mainController.js
+++ app/components/controllers/mainController.js
@@ -1,7 +1,35 @@
-﻿(function(module) {
+(function(module) {
+
+	function Cat(name, imgsrc) {
+		this.name = name;
+		this.imgsrc = imgsrc;
+		this.viewed = false;
+		this.clicks = 0;
+		this.votes = 0;
+	}
 
     var mainController = function ($scope) {
-        $scope.text = "Hello World?";
+    	$scope.cats = [
+    	    new Cat("Bonnie and Clyde", "/app/img/cat1-720.jpg"),
+    	    new Cat("Indiana Jones", "/app/img/cat2-720.jpg"),
+    	    new Cat("Batman", "/app/img/cat3-720.jpg"),
+    	    new Cat("Cristmas Elfs", "/app/img/cat4-720.jpg"),
+    	    new Cat("Fluffy Cat", "/app/img/cat5-720.jpg")
+    	];
+    	$scope.selectCat = function(cat) {
+    		cat.viewed = true;
+    		$scope.selectedCat = cat;
+    	};
+    	$scope.reverse = '1';
+        $scope.positiveVotes = 0;
+        // $scope.$watch(
+        //     "cats",
+        //     function( newValue, oldValue ) {
+        //         addLogItem( $scope.watchLog );
+        //     }
+        // );
+        $scope.action = 'view';
+        $scope.newCat = new Cat();
     };
 
     module.controller("mainController", mainController);
