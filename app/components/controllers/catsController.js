210645681c4edf50fe7703e48f7d4cd5a97c7a5a
unknown <Valery_Tadra@EPBYMINW0255.minsk.epam.com>
2016-03-28 14:05:23 +0300

    Cat app

---
+++ app/components/controllers/catsController.js
@@ -0,0 +1,49 @@
+(function(module) {
+
+    var catsController = function ($scope) {
+        $scope.text = "Hello World?";
+        $scope.noClicks = 0;
+        $scope.searchFilter = "";
+        $scope.activeSearchFilter = "";
+
+        $scope.cats = [
+            {catImg: 'cat1.png', catName: 'zcat1', noClicks: 0, votes: 0, visited: false},
+            {catImg: 'cat2.png', catName: 'fcat2', noClicks: 0, votes: 0, visited: false},
+            {catImg: 'cat3.png', catName: 'cat3', noClicks: 0, votes: 0, visited: false},
+            {catImg: 'cat4.png', catName: 'kcat4', noClicks: 0, votes: 0, visited: false},
+            {catImg: 'cat5.png', catName: 'cat5', noClicks: 0, votes: 0, visited: false},
+            {catImg: 'cat6.png', catName: 'cat6', noClicks: 0, votes: 0, visited: false}
+        ];
+
+
+        //$scope.selectedCat = $scope.cats[0];
+
+        $scope.selectCat = function (cat) {
+            $scope.selectedCat = cat;
+            cat.visited = true;
+        };
+
+        $scope.addVote = function (cat) {
+
+            cat.votes++;
+        }
+
+        $scope.removeVote = function (cat) {
+
+            cat.votes--;
+        }
+        
+        $scope.setActiveSearchFilter = function(searchBy) {
+            $scope.activeSearchFilter = searchBy;
+        }
+
+        $scope.setNewCatImgFinal = function(imgSrc) {
+
+            
+            $scope.newCatImgFinal = imgSrc;
+        }
+    };
+
+    module.controller("catsCtrl", catsController);
+
+}(angular.module("app")));
