0025f7f73d29278c3a04b6e191c9ed59bcb7baf8
letsiaha <letsiaha@gmail.com>
2016-03-28 16:54:39 +0300

    inital commit hometasks 1 and 2

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -9,6 +9,8 @@ angular.module('myApp.view2', ['ngRoute'])
   });
 }])
 
-.controller('View2Ctrl', [function() {
-
+.controller('View2Ctrl', ['$scope', function($scope) {
+  $scope.addNew = function (newCat) {
+    console.log(newCat);
+  }
 }]);
