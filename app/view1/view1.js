795ceeca465b02da9894a4c482a88407e22586ac
letsiaha <letsiaha@gmail.com>
2016-04-01 10:01:35 +0300

    service works

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,46 +9,52 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', ['$scope', function($scope) {
-  
-  $scope.cats = [
-    {
-      name: 'Senior',
-      image: 'img/grooming-needs-senior-cat.jpg',
-      clicks: 0,
-      wasSelected: false
-    },
-    {
-      name: 'Black',
-      image: 'img/black.jpg',
-      clicks: 0,
-      wasSelected: false
-    },
-    {
-      name: 'Bengal',
-      image: 'img/bengal.jpg',
-      clicks: 0,
-      wasSelected: false
-    },
-    {
-      name: 'Flash',
-      image: 'img/flash.jpg',
-      clicks: 0,
-      wasSelected: false
-    },
-    {
-      name: 'Skull',
-      image: 'img/skull.jpg',
-      clicks: 0,
-      wasSelected: false
-    },
-    {
-      name: 'Bread',
-      image: 'img/breading.jpg',
-      clicks: 0,
-      wasSelected: false
-    }
-  ];
+.controller('View1Ctrl', ['$scope', 'CatService', function($scope, CatService) {
+  //  $http.get('http://localhost:8000/app/cats.json').then(function (d) {
+  //    //$scope.cats = d;
+  //    console.log(d);
+  // });
+
+  $scope.cats = CatService.getCats();
+
+  // $scope.cats = [
+  //   {
+  //     name: 'Senior',
+  //     image: 'img/grooming-needs-senior-cat.jpg',
+  //     clicks: 0,
+  //     wasSelected: false
+  //   },
+  //   {
+  //     name: 'Black',
+  //     image: 'img/black.jpg',
+  //     clicks: 0,
+  //     wasSelected: false
+  //   },
+  //   {
+  //     name: 'Bengal',
+  //     image: 'img/bengal.jpg',
+  //     clicks: 0,
+  //     wasSelected: false
+  //   },
+  //   {
+  //     name: 'Flash',
+  //     image: 'img/flash.jpg',
+  //     clicks: 0,
+  //     wasSelected: false
+  //   },
+  //   {
+  //     name: 'Skull',
+  //     image: 'img/skull.jpg',
+  //     clicks: 0,
+  //     wasSelected: false
+  //   },
+  //   {
+  //     name: 'Bread',
+  //     image: 'img/breading.jpg',
+  //     clicks: 0,
+  //     wasSelected: false
+  //   }
+  // ];
 
   //$scope.clicks = 0;
 
