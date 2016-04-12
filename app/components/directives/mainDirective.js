321d7126f3cb561d027bd60e61130f4ea8d62cd9
Uladzislau Stasevich <Uladzislau_Stasevich@epam.com>
2016-04-12 08:01:57 +0300

    [3] Directives - CatViewer - advanced (Step 1.3)

--- app/components/directives/mainDirective.js
+++ app/components/directives/mainDirective.js
@@ -23,8 +23,29 @@
 		}
 	};
 
+	var picture = function() {
+		return {
+			restrict: 'E',
+			template: '<img ng-click="count(currentCat)" ng-src="{{currentCat.src}}"/>' +
+			'<span class="counter">{{currentCat.counter ? currentCat.counter : 0}}</span>' +
+			'<h2>{{currentCat.name}}</h2>'
+		}
+	};
+	
+	var pictUrl = function() {
+		return {
+			restrict: 'E',
+			template: '<input type="url" name="pictUrl" placeholder="Picture Url"' +
+			'ng-change="currentCat.isMessageVisible = false"' +
+			'ng-model="currentCat.draftPictUrl"' +
+			'ng-blur="saveUrl(currentCat)" />'
+		}
+	};
 
-	module.directive("focusedElement", focusedElement);
-	module.directive("pressedEnter", pressedEnter);
 
-}(angular.module("app")));
+	module.directive('focusedElement', focusedElement);
+	module.directive('pressedEnter', pressedEnter);
+	module.directive('picture', picture);
+	module.directive('pictUrl', pictUrl)
+
+}(angular.module('app')));
