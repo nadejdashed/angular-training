62233e0b16d17cc94386f80b39ae21ba65ee0bd9
xtinakot <xtinakot@gmail.com>
2016-05-24 21:58:53 +0300

    Final code review

--- app/components/directives/enterKeyDirective.js
+++ app/components/directives/enterKeyDirective.js
@@ -3,11 +3,14 @@
         return {
             restrict: "A",
             link: function(scope, element, attrs) {
+                // todo: remove excess code, there is $document service already
                 angular.element(document).on('keypress', function(e) {
                     if (e.keyCode == 13) {
                         element[0].click();
                     }
                 });
+                // memory leak!
+                // todo: unsubscribe from the event on scope destroy
             }
         };
     });
