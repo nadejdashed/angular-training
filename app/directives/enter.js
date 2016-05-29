330feed966f6838198f9d4f7b1c8f272c6f3d96f
xtinakot <xtinakot@gmail.com>
2016-05-29 21:35:40 +0300

    Final code review

--- app/directives/enter.js
+++ app/directives/enter.js
@@ -10,6 +10,7 @@ angular.module('myApp')
             },
             link: function ($scope, element, attrs) {
 
+                // todo: unsubscribe from the event in order not to introduce memory leaks on scope destroy
                 $document.on('keydown', function(ev) {
                     if (ev.keyCode != 13) return;
 
